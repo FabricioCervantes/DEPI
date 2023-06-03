476a2b257a0431ada50562cbaca6a4d0d3ac4e94
Fabricio Cervantes <marcofabri6@gmail.com>
2023-06-02 20:03:47 -0600

    final

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img srcset="{{ asset('/build/assets/img/logo_tec.png') }}" class="h-40 mt-10" alt="">
+    <img srcset="{{ asset('/build/assets/img/logo_tec.png') }}" class="h-[20vh] mt-10" alt="">
 </a>
