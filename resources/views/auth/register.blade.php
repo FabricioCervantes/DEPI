7a2e012747be8af80e1205fcaf2d42b7a925f9eb
Fabricio Cervantes <marcofabri6@gmail.com>
2023-06-03 08:00:45 -0600

    mensajes de error en form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,19 +11,20 @@
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
+
             <div>
                 <x-label for="name" value="{{ __('Name') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" autofocus
-                    autocomplete="name" />
+                <x-input id="name" class="block mt-1 w-full" required type="text" name="name" :value="old('name')"
+                    autofocus autocomplete="name" />
                 @error('name')
-                    <span class="text-red-500 text-sm">El campo es obligatorio.</span>
+                    <span class="text-red-500 text-sm"></span>
                 @enderror
             </div>
 
             <div class="mt-4">
                 <x-label for="lastName" value="{{ __('Apellidos') }}" />
-                <x-input id="lastName" class="block mt-1 w-full" type="text" name="lastName" :value="old('lastName')"
-                    autofocus autocomplete="lastName" />
+                <x-input id="lastName" required class="block mt-1 w-full" type="text" name="lastName"
+                    :value="old('lastName')" autofocus autocomplete="lastName" />
                 @error('lastName')
                     <span class="text-red-500 text-sm">El campo es obligatorio.</span>
                 @enderror
@@ -31,11 +32,10 @@
 
             <div class="mt-4">
                 <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                    autocomplete="username" />
+                <x-input id="email" required class="block mt-1 w-full" type="email" name="email"
+                    :value="old('email')" autocomplete="username" />
                 @error('email')
-                    <span class="text-red-500 text-sm">El campo es obligatorio y debe ser una dirección de correo
-                        electrónico.</span>
+                    <span class="text-red-500 text-sm">Recuerda que el correo debe ser institucional</span>
                 @enderror
             </div>
 
@@ -43,10 +43,10 @@
                 <x-label for="tipou" value="{{ __('Tipo de usuario') }}" />
                 <select
                     class="border-gray-300 w-full mt-1 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                    name="tipou" id="tipou">
+                    name="tipou" id="tipou" required>
                     <option value="">Seleccione...</option>
-                    <option value="E">Estudiante</option>
-                    <option value="A">Academico</option>
+                    <option {{ old('tipou') == 'E' ? 'selected' : '' }} value="E">Estudiante</option>
+                    <option {{ old('tipou') == 'A' ? 'selected' : '' }} value="A">Academico</option>
                 </select>
                 @error('tipou')
                     <span class="text-red-500 text-sm">El campo es obligatorio.</span>
@@ -57,7 +57,7 @@
             <div class="flex gap-5 justify-between mt-4">
                 <div class="w-full">
                     <x-label for="nacimiento" value="{{ __('Fecha de nacimiento') }}" />
-                    <x-input id="nacimiento" class="block mt-1 w-full" type="date" name="nacimiento"
+                    <x-input id="nacimiento" required class="block mt-1 w-full" type="date" name="nacimiento"
                         :value="old('nacimiento')" autofocus autocomplete="nacimiento" />
                     @error('nacimiento')
                         <span class="text-red-500 text-sm">El campo es obligatorio.</span>
@@ -67,10 +67,10 @@
                     <x-label for="sexo" value="{{ __('Sexo') }}" />
                     <select
                         class="border-gray-300 w-full mt-1 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                        name="sexo">
+                        name="sexo" required>
                         <option value="">Seleccione...</option>
-                        <option value="Hombre">Hombre</option>
-                        <option value="Mujer">Mujer</option>
+                        <option {{ old('sexo') == 'Hombre' ? 'selected' : '' }} value="Hombre">Hombre</option>
+                        <option {{ old('sexo') == 'Mujer' ? 'selected' : '' }} value="Mujer">Mujer</option>
                     </select>
                     @error('sexo')
                         <span class="text-red-500 text-sm">El campo es obligatorio.</span>
@@ -99,8 +99,7 @@
                     <x-input id="conacyt" class="block mt-1 w-full" type="number" name="conacyt" :value="old('conacyt')"
                         autocomplete="conacyt" />
                     @error('conacyt')
-                        <span class="text-red-500 text-sm">El campo es obligatorio y debe ser un número de control
-                            válido.</span>
+                        <span class="text-red-500 text-sm">El campo es obligatorio</span>
                     @enderror
                 </div>
 
@@ -109,8 +108,7 @@
                     <x-input id="cvu" class="block mt-1 w-full" type="text" name="cvu" :value="old('cvu')"
                         autocomplete="cvu" />
                     @error('cvu')
-                        <span class="text-red-500 text-sm">El campo es obligatorio y debe ser un número de control
-                            válido.</span>
+                        <span class="text-red-500 text-sm">El campo es obligatorio.</span>
                     @enderror
                 </div>
 
@@ -118,22 +116,19 @@
 
             <div class="mt-4 w-full">
                 <x-label for="telefono" value="{{ __('Teléfono') }}" />
-                <x-input id="telefono" class="block mt-1 w-full" type="number" name="telefono" :value="old('telefono')"
-                    autocomplete="telefono" />
+                <x-input id="telefono" required class="block mt-1 w-full" type="number" name="telefono"
+                    :value="old('telefono')" autocomplete="telefono" />
                 @error('telefono')
                     <span class="text-red-500 text-sm">El campo es obligatorio y debe ser un número de teléfono.</span>
                 @enderror
             </div>
-
-
-
 
             <div class="flex gap-5 justify-between mt-4">
                 <div class="w-full">
                     <x-label for="institucion" value="{{ __('Institución') }}" />
                     <select
                         class="border-gray-300 w-full mt-1 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                        name="institucion">
+                        name="institucion" required>
                         <option value="">Seleccione...</option>
                         @foreach ($instituciones as $item)
                             <option value="{{ $item->nombre }}">{{ $item->nombre }}</option>
@@ -147,32 +142,53 @@
                     <x-label for="carrera" value="{{ __('Carrera o Postgrado') }}" />
                     <select
                         class="border-gray-300 w-full mt-1 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                        name="carrera">
+                        name="carrera" required>
                         <option value="">Seleccione una opción...</option>
-                        <option value="Ingenieria Mecatrónica">Ingenieria Mecatrónica</option>
-                        <option value="Ingenieria en Sistemas Computacionales">Ingenieria en Sistemas Computacionales
+                        <option {{ old('carrera') == 'Ingenieria Mecatrónica' ? 'selected' : '' }}
+                            value="Ingenieria Mecatrónica">
+                            Ingenieria Mecatrónica</option>
+                        <option {{ old('carrera') == 'Ingenieria en Sistemas Computacionales' ? 'selected' : '' }}
+                            value="Ingenieria en Sistemas Computacionales">Ingenieria en Sistemas Computacionales
                         </option>
-                        <option value="Ingenieria Eléctrica">Ingenieria Eléctrica</option>
-                        <option value="Ingenieria Bioquimica">Ingenieria Bioquimica</option>
-                        <option value="Ingenieria Industrial">Ingenieria Industrial</option>
-                        <option value="Ingenieria en Materiales">Ingenieria en Materiales</option>
-                        <option value="Ingenieria Mecánica">Ingenieria Mecánica</option>
-                        <option value="Ingenieria Eléctrónica">Ingenieria Eléctrónica</option>
-                        <option value="Ingenieria en Gestión Empresarial">Ingenieria en Gestión Empresarial</option>
-                        <option value="Maestría en Ingenieria Administrativa">Maestría en Ingenieria Administrativa
+                        <option {{ old('carrera') == 'Ingenieria Eléctrica' ? 'selected' : '' }}
+                            value="Ingenieria Eléctrica">Ingenieria Eléctrica</option>
+                        <option {{ old('carrera') == 'Ingenieria Bioquimica' ? 'selected' : '' }}
+                            value="Ingenieria Bioquimica">Ingenieria Bioquimica</option>
+                        <option {{ old('carrera') == 'Ingenieria Industrial' ? 'selected' : '' }}
+                            value="Ingenieria Industrial">Ingenieria Industrial</option>
+                        <option {{ old('carrera') == 'Ingenieria en Materiales' ? 'selected' : '' }}
+                            value="Ingenieria en Materiales">Ingenieria en Materiales</option>
+                        <option {{ old('carrera') == 'Ingenieria Mecánica' ? 'selected' : '' }}
+                            value="Ingenieria Mecánica">Ingenieria Mecánica</option>
+                        <option {{ old('carrera') == 'Ingenieria Eléctrónica' ? 'selected' : '' }}
+                            value="Ingenieria Eléctrónica">Ingenieria Eléctrónica</option>
+                        <option {{ old('carrera') == 'Ingenieria en Gestión Empresarial' ? 'selected' : '' }}
+                            value="Ingenieria en Gestión Empresarial">Ingenieria en Gestión Empresarial</option>
+                        <option {{ old('carrera') == 'Maestría en Ingenieria Administrativa' ? 'selected' : '' }}
+                            value="Maestría en Ingenieria Administrativa">Maestría en Ingenieria Administrativa
                         </option>
-                        <option value="Maestría en Ingenieria Eléctrica">Maestría en Ingenieria Eléctrica</option>
-                        <option value="Maestría en Sistemas Computacionales">Maestría en Sistemas Computacionales
-                        </option>
-                        <option value="Maestría en Ciencias en Ingenieria Eléctrica">Maestría en Ciencias en Ingenieria
+                        <option {{ old('carrera') == 'Maestría en Ingenieria Eléctrica' ? 'selected' : '' }}
+                            value="Maestría en Ingenieria Eléctrica">Maestría en Ingenieria Eléctrica</option>
+                        <option {{ old('carrera') == 'Maestría en Sistemas Computacionales' ? 'selected' : '' }}
+                            value="Maestría en Sistemas Computacionales">Maestría en Sistemas Computacionales</option>
+                        <option
+                            {{ old('carrera') == 'Maestría en Ciencias en Ingenieria Eléctrica' ? 'selected' : '' }}
+                            value="Maestría en Ciencias en Ingenieria Eléctrica">Maestría en Ciencias en Ingenieria
                             Eléctrica</option>
-                        <option value="Maestría en Ciencias en Ingenieria Electrónica">Maestría en Ciencias en
-                            Ingenieria Electrónica</option>
-                        <option value="Maestría en Ciencias en Metalurgia">Maestría en Ciencias en Metalurgia</option>
-                        <option value="Doctorado en Ciencias de la Ingenieria">Doctorado en Ciencias de la Ingenieria
+                        <option
+                            {{ old('carrera') == 'Maestría en Ciencias en Ingenieria Electrónica' ? 'selected' : '' }}
+                            value="Maestría en Ciencias en Ingenieria Electrónica">Maestría en Ciencias en Ingenieria
+                            Electrónica</option>
+                        <option {{ old('carrera') == 'Maestría en Ciencias en Metalurgia' ? 'selected' : '' }}
+                            value="Maestría en Ciencias en Metalurgia">Maestría en Ciencias en Metalurgia</option>
+                        <option {{ old('carrera') == 'Doctorado en Ciencias de la Ingenieria' ? 'selected' : '' }}
+                            value="Doctorado en Ciencias de la Ingenieria">Doctorado en Ciencias de la Ingenieria
                         </option>
-                        <option value="Doctorado en Ciencias en Ingenieria Eléctrica">Doctorado en Ciencias en
-                            Ingenieria Eléctrica</option>
+                        <option
+                            {{ old('carrera') == 'Doctorado en Ciencias en Ingenieria Eléctrica' ? 'selected' : '' }}
+                            value="Doctorado en Ciencias en Ingenieria Eléctrica">Doctorado en Ciencias en Ingenieria
+                            Eléctrica</option>
+
                     </select>
                     @error('carrera')
                         <span class="text-red-500 text-sm">El campo es obligatorio.</span>
@@ -182,7 +198,9 @@
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password"
+                <x-label class="text-red-800"
+                    value="La contraseña debe llevar mínimo 8 caracteres, un número y un carácter especial" />
+                <x-input id="password" required class="block mt-1 w-full" type="password" name="password"
                     autocomplete="new-password" />
                 @error('password')
                     <span class="text-red-500 text-sm">El campo es obligatorio.</span>
@@ -192,7 +210,7 @@
             <div class="mt-4">
                 <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
                 <x-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                    name="password_confirmation" autocomplete="new-password" />
+                    name="password_confirmation" required autocomplete="new-password" />
                 @error('password_confirmation')
                     <span class="text-red-500 text-sm">El campo es obligatorio.</span>
                 @enderror
