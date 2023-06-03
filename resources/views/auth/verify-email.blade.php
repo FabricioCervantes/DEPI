e5adfaa2ec72971228fd91990aebddd67cd52731
Fabricio Cervantes <marcofabri6@gmail.com>
2023-06-02 20:20:50 -0600

    final

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -16,7 +16,7 @@
             </div>
         @endif
 
-        <div class="mt-4 flex items-center justify-between">
+        <div class="mt-4 flex flex-col md:flex-row items-center justify-between">
             <form method="POST" action="{{ route('verification.send') }}">
                 @csrf
 
